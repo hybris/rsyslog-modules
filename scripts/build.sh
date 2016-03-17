5bad603515e61cba9703439df10ad9e2eb52bb52
Louis-Philippe Bedard <louis-philippe.bedard@sap.com>
2016-03-17 15:38:26 -0400

    Fix output path

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,21 +20,21 @@ sh autogen.sh
 make && make install
 
 
-git clone https://github.com/rsyslog/rsyslog.git /${output_folder}
-cd /${output_folder}
+git clone https://github.com/rsyslog/rsyslog.git /tmp/rsyslog
+cd /tmp/rsyslog
 latest_rsyslog_tag=`git tag --sort=-v:refname --list | head -1`
 
 cd $DIR
 
 if [ "x$( git tag --list ${latest_rsyslog_tag} )" = "x" ]; then
 
-  cd /${output_folder}
+  cd /tmp/rsyslog
 
   git checkout -b ${latest_rsyslog_tag} refs/tags/${latest_rsyslog_tag}
-  ./autogen.sh --enable-omkafka --disable-generate-man-pages --prefix/=${output_folder}
+  ./autogen.sh --enable-omkafka --disable-generate-man-pages --prefix=/tmp/rsyslog
   make && make install
 
-  # cp /${output_folder}/lib/rsyslog/omkafka.so ${output_folder}
+  # cp /tmp/rsyslog/lib/rsyslog/omkafka.so ${output_folder}
   # cp $ROOT/$rsyslog_release/tag ${output_folder}
   # cp $ROOT/$rsyslog_release/tag ${output_folder}/name
   # touch ${output_folder}/note.md
@@ -51,7 +51,7 @@ if [ "x$( git tag --list ${latest_rsyslog_tag} )" = "x" ]; then
     --repo rsyslog-modules \
     --tag ${latest_rsyslog_tag} \
     --name "omkakfa.so" \
-    --file ${output_folder}
+    --file /tmp/rsyslog
 
 
 fi

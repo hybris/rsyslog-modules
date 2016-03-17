8e7f07366d0c5bf567841725a7ae7ec77e2ba5a3
Louis-Philippe Bedard <louis-philippe.bedard@sap.com>
2016-03-17 15:37:23 -0400

    Fix output path

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,21 +20,21 @@ sh autogen.sh
 make && make install
 
 
-git clone https://github.com/rsyslog/rsyslog.git ${output_folder}
-cd ${output_folder}
+git clone https://github.com/rsyslog/rsyslog.git /${output_folder}
+cd /${output_folder}
 latest_rsyslog_tag=`git tag --sort=-v:refname --list | head -1`
 
 cd $DIR
 
 if [ "x$( git tag --list ${latest_rsyslog_tag} )" = "x" ]; then
 
-  cd ${output_folder}
+  cd /${output_folder}
 
   git checkout -b ${latest_rsyslog_tag} refs/tags/${latest_rsyslog_tag}
-  ./autogen.sh --enable-omkafka --disable-generate-man-pages --prefix=${output_folder}
+  ./autogen.sh --enable-omkafka --disable-generate-man-pages --prefix/=${output_folder}
   make && make install
 
-  # cp ${output_folder}/lib/rsyslog/omkafka.so ${output_folder}
+  # cp /${output_folder}/lib/rsyslog/omkafka.so ${output_folder}
   # cp $ROOT/$rsyslog_release/tag ${output_folder}
   # cp $ROOT/$rsyslog_release/tag ${output_folder}/name
   # touch ${output_folder}/note.md
